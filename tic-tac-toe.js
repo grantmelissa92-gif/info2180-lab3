9fa4e19d5c6a99aa72eb422cc6f0040d241adf6d
Melissa Grant <grantmelissa92@gmail.com>
2025-10-10 13:27:02 -0500

    "Added X and O to squares"

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,31 @@
-//Layout the board 
 document.addEventListener("DOMContentLoaded", function(){
     //game board
     const board = document.getElementById("board");
-
     //each square in the grid
     const square = board.getElementsByTagName("div");
+
+    //set player and turn
+    const player1 = "X";
+    const player2 = "O";
+    let current_turn = player1;
+
+    let gameXorO = Array(square.length).fill("");
+
     for(let a = 0; a < square.length; a++){
         square[a].classList.add("square");
-    }
 
+        square[a].addEventListener("click", function(){
+            if (gameXorO[a] === ""){
+                gameXorO[a] = current_turn;
+
+                square[a].textContent = current_turn;
+
+                square[a].classList.add(current_turn);
+
+                current_turn = current_turn === player1 ? player2 : player1;
+            }
+        });
+
+    } 
 });
+
